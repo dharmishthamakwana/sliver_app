39d0203366d8e8ce3abc62ecaf6c2179bb4a92ed
Admin <makwanadharmishtha44@gmail.com>
2023-04-28 11:56:13 +0530

    kids game data table

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:sliver_app/screens/home/view/fifth_screen.dart';
-import 'package:sliver_app/screens/home/view/forth_screen.dart';
-import 'package:sliver_app/screens/home/view/home_Screen.dart';
-import 'package:sliver_app/screens/home/view/second_Screen.dart';
-import 'package:sliver_app/screens/home/view/third_Screen.dart';
+import 'package:sliver_app/screens/home/view/kids_game2.dart';
+import 'package:sliver_app/screens/home/view/Kids_game.dart';
+import 'package:sliver_app/screens/home/view/sliver_screen1.dart';
+import 'package:sliver_app/screens/home/view/sliver_screen2.dart';
+import 'package:sliver_app/screens/home/view/sliver_Screen3.dart';
+
+import 'screens/home/view/DataTable.dart';
 
 void main() {
   runApp(
     GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: 'third',
+      initialRoute: 'sixth',
       routes: {
         '/': (p0) => HomeScreen(),
         'second': (p0) => SecondScreen(),
-        // 'third': (p0) => ThirdScreen(),
-        'third': (p0) => ForthScreen(),
-        'forth': (p0) => Home(),
+        'third': (p0) => ThirdScreen(),
+        'Forth': (p0) => ForthScreen(),
+        'Fifth': (p0) => Home(),
+        'sixth':(p0) =>DataScreen(),
       },
     ),
   );
---
+++ lib/screens/home/view/DataTable.dart
@@ -0,0 +1,159 @@
+import 'dart:html';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class DataScreen extends StatefulWidget {
+  const DataScreen({Key? key}) : super(key: key);
+
+  @override
+  State<DataScreen> createState() => _DataScreenState();
+}
+
+class _DataScreenState extends State<DataScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.amber.shade50,
+        appBar: AppBar(
+          centerTitle: true,
+            title: Text("Data Table",
+
+                style: TextStyle(
+                    color: Colors.black, fontWeight: FontWeight.bold,fontSize: 25,letterSpacing: 1)),
+            backgroundColor: Colors.amber.shade900),
+        body: Center(
+          child: DataTable(
+            border: TableBorder.all(color: Colors.black, width: 1),
+            headingRowColor: MaterialStateProperty.all(Colors.amber),
+            dataRowColor: MaterialStatePropertyAll(Colors.amber.shade100),
+            headingTextStyle: TextStyle(
+                color: Colors.black, fontWeight: FontWeight.bold, fontSize: 25),
+            columns: [
+              DataColumn(
+                label: Text("ID"),
+              ),
+              DataColumn(
+                label: Text("Name"),
+              ),
+              DataColumn(
+                label: Text("Course"),
+              ),
+              DataColumn(
+                label: Text("Mobile"),
+              ),
+            ],
+            rows: [
+              DataRow(
+                cells: [
+                  DataCell(Text(
+                    "1",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  )),
+                  DataCell(
+                    Text(
+                      "dharmishtha",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  DataCell(
+                    Text(
+                      "BCA",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  DataCell(
+                    Text(
+                      "9925841343",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+              DataRow(cells: [
+                DataCell(
+                  Text(
+                    "2",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                DataCell(
+                  Text(
+                    "dharmi",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                DataCell(
+                  Text(
+                    "BCA",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                DataCell(
+                  Text(
+                    "9725150259",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ]),
+              DataRow(cells: [
+                DataCell(
+                  Text(
+                    "3",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                DataCell(
+                  Text(
+                    "Prina",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                DataCell(
+                  Text(
+                    "BCA",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                DataCell(
+                  Text(
+                    "9922885533",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ]),
+              DataRow(cells: [
+                DataCell(
+                  Text(
+                    "4",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                DataCell(
+                  Text(
+                    "Ishu",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                DataCell(
+                  Text(
+                    "BCA",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                DataCell(
+                  Text(
+                    "8844776633",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ]),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
